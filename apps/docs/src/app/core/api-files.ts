5c6d9661ed0fac19fe94bb14dc9114ef936ead6e
InnaAtanasova <i.atanasova@sap.com>
2021-04-20 18:06:22 -0400

    add examples

--- apps/docs/src/app/core/api-files.ts
+++ apps/docs/src/app/core/api-files.ts
@@ -205,10 +205,8 @@ export const API_FILES = {
         'NotificationHeaderComponent',
         'NotificationBodyComponent',
         'NotificationFooterComponent',
-        'DefaultNotificationComponent',
         'NotificationContainer',
         'NotificationConfig',
-        'NotificationDefault',
         'NotificationRef'
     ],
     objectIdentifier: ['ObjectIdentifierComponent'],
